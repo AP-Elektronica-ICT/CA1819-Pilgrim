c14d23bb727afaf00b067e88cf5e487e602d846a
Unknown <midasvg@hotmail.com>
2018-10-10 12:10:52 +0200

    Camera Permission

--- AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.example.midasvg.pilgrim;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,28 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        checkCameraPermission();
     }
+
+
+    private void checkCameraPermission(){
+        if(ContextCompat.checkSelfPermission(LoginActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            //Permission is not granted
+            if(ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this,
+                    Manifest.permission.CAMERA)){
+
+            }else{
+                ActivityCompat.requestPermissions(LoginActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        }
+    }
+
+
 
 
 
