7028b54b49e78f2c7677d17950da87febb33dd31
Unknown <midasvg@hotmail.com>
2018-10-15 18:58:51 +0200

    Android App can open unity app

--- AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/GameActivity.java
+++ AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/GameActivity.java
@@ -1,7 +1,10 @@
 package com.example.midasvg.pilgrim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final Button openCamera = (Button) findViewById(R.id.bttnCamera);
+        openCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.issam.Tutorial");
+                startActivity(intent);
+            }
+        });
     }
 }
