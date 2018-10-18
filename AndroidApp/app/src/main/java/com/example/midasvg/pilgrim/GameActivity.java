8d98a94b2fc7e919c9492146170630a46bcee3c8
Unknown <midasvg@hotmail.com>
2018-10-18 14:30:31 +0200

    Alert dialog when user tries to quit.

--- AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/GameActivity.java
+++ AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/GameActivity.java
@@ -1,6 +1,8 @@
 package com.example.midasvg.pilgrim;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +21,34 @@ public class GameActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = getPackageManager().getLaunchIntentForPackage("com.issam.PilgrimAr");
                 startActivity(intent);
+            }
+        });
+
+        final Button quitGame = (Button) findViewById(R.id.bttnQuit);
+        quitGame.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Are you sure you want to quit?");
+                builder.setMessage("The progress you've made will be deleted & you will not recieve any points!");
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Quit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(GameActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
             }
         });
     }
