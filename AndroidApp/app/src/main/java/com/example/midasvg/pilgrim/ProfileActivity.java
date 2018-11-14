a626c42d063fadef7b50feec134c30f73f5f611a
Unknown <midasvg@hotmail.com>
2018-11-14 10:56:44 +0100

    User can sign out

--- AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/ProfileActivity.java
+++ AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/ProfileActivity.java
@@ -1,7 +1,13 @@
 package com.example.midasvg.pilgrim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        final Button signOut = (Button) findViewById(R.id.logoutButton);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "You are now signed out.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
