700b66aa988e3a0ffe086d780667ef4e0d14b3a0
Midas Van Gucht <midasvg@hotmail.com>
2018-11-20 15:27:14 +0100

    Calls are made from Android to API

--- AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/GameActivity.java
+++ AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/GameActivity.java
@@ -24,9 +24,14 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -184,14 +189,32 @@ public class GameActivity extends AppCompatActivity {
         }
 
         //API aanspreken
+        String locationURL = "http://localhost:44384/locations";
+
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         JsonObjectRequest objectRequest = new JsonObjectRequest(
                 Request.Method.GET,
+                locationURL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Locaties ophalen
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("REST response", error.toString());
+                    }
+                }
+
         );
+        requestQueue.add(objectRequest);
 
 
-    }
+}
 
     @Override
     public void onBackPressed() {
--- AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/ProfileActivity.java
+++ AndroidApp/app/src/main/java/com/example/midasvg/pilgrim/ProfileActivity.java
@@ -3,11 +3,20 @@ package com.example.midasvg.pilgrim;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -30,6 +39,33 @@ public class ProfileActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
+
+
+        //API aanspreken
+        String locationURL = "http://localhost:44384/locations";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                locationURL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //profile picture ophalen
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("REST response", error.toString());
+                    }
+                }
+
+        );
+        requestQueue.add(objectRequest);
 
     }
 }
